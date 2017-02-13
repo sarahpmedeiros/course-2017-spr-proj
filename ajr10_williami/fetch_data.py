ae6a0d350c6c86d172b5f716673b55840d09010d
williamieong <ieong24861@yahoo.com>
2017-02-13 13:45:07 -0500

    Added providence for boston open spaces and trees

--- ajr10_williami/fetch_data.py
+++ ajr10_williami/fetch_data.py
@@ -126,19 +126,27 @@ class fetch_data(dml.Algorithm):
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('cdp', 'https://data.cambridgema.gov/resource/')
+        doc.add_namespace('bod', 'http://bostonopendata-boston.opendata.arcgis.com/datasets/')
 
         this_script = doc.agent('alg:ajr10_williami#fetch_data', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         open_spaces_cambridge_resource = doc.entity('cdp:5ctr-ccas', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         playgrounds_cambridge_resource = doc.entity('cdp:wagv-xsb4', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         trees_cambridge_resource = doc.entity('cdp:q83f-7quz', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        
+        open_spaces_boston_resource = doc.entity('bod:2868d370c55d4d458d4ae2224ef8cddd_7.geojson', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        trees_boston_resource = doc.entity('bod:ce863d38db284efe83555caf8a832e2a_1.geojson', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+
+
         get_open_spaces_cambridge = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_playgrounds_cambridge = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_trees_cambridge = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_open_spaces_boston = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_trees_boston = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         
         doc.wasAssociatedWith(get_open_spaces_cambridge, this_script)
         doc.wasAssociatedWith(get_playgrounds_cambridge, this_script)
         doc.wasAssociatedWith(get_trees_cambridge, this_script)
+        doc.wasAssociatedWith(get_open_spaces_boston, this_script)
+        doc.wasAssociatedWith(get_trees_boston, this_script)
 
         doc.usage(get_open_spaces_cambridge, open_spaces_cambridge_resource, startTime, None,
                   {prov.model.PROV_TYPE:'ont:Retrieval',
@@ -156,6 +164,16 @@ class fetch_data(dml.Algorithm):
                   'ont:Query':'?type=Trees+Cambridge'
                   }
                   )
+        doc.usage(get_open_spaces_boston, open_spaces_boston_resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Open+Spaces+Boston'
+                  }
+                  )
+        doc.usage(get_trees_boston, trees_boston_resource, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Trees+Boston'
+                  }
+                  )
 
         open_spaces_cambridge = doc.entity('dat:ajr10_williami#open_spaces_cambridge', {prov.model.PROV_LABEL:'Open spaces Cambridge', prov.model.PROV_TYPE:'ont:DataSet'})
         doc.wasAttributedTo(open_spaces_cambridge, this_script)
@@ -171,6 +189,17 @@ class fetch_data(dml.Algorithm):
         doc.wasAttributedTo(trees_cambridge, this_script)
         doc.wasGeneratedBy(trees_cambridge, get_trees_cambridge, endTime)
         doc.wasDerivedFrom(trees_cambridge, trees_cambridge_resource, get_trees_cambridge, get_trees_cambridge, get_trees_cambridge)
+
+        open_spaces_boston = doc.entity('dat:ajr10_williami#open_spaces_boston', {prov.model.PROV_LABEL:'Open spaces Boston', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(open_spaces_boston, this_script)
+        doc.wasGeneratedBy(open_spaces_boston, get_open_spaces_boston, endTime)
+        doc.wasDerivedFrom(open_spaces_boston, open_spaces_boston_resource, get_open_spaces_boston, get_open_spaces_boston, get_open_spaces_boston)
+
+        trees_boston = doc.entity('dat:ajr10_williami#trees_boston', {prov.model.PROV_LABEL:'Trees Boston', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(trees_boston, this_script)
+        doc.wasGeneratedBy(trees_boston, get_trees_boston, endTime)
+        doc.wasDerivedFrom(trees_boston, trees_boston_resource, get_trees_boston, get_trees_boston, get_trees_boston)
+
 
         repo.logout()
                   
