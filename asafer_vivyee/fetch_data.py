6d5e3bc354c20eb382efac1aa23ab455b21c3291
veev <vivyee@bu.edu>
2017-02-14 18:13:34 -0500

    work on stuff

--- asafer_vivyee/fetch_data.py
+++ asafer_vivyee/fetch_data.py
@@ -11,14 +11,28 @@ class FetchData(dml.Algorithm):
     writes = ['asafer_vivyee.orchards', 'asafer_vivyee.corner_stores', 'asafer_vivyee.obesity', 'asafer_vivyee.nutrition_prog', 'asafer_vivyee.mbta']
 
     @staticmethod
-    def execute(trial = False):
-        '''Retrieve some data sets'''
-        startTime = datetime.datetime.now()
-
+    def setup():
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('asafer_vivyee', 'asafer_vivyee')
+        return repo
+
+    @staticmethod
+    def store(repo, url, collection):
+        response = urllib.request.urlopen(url).read().decode("utf-8")
+        if response.status_code == 200:
+            data = json.loads(response)
+            repo.dropPermanent(collection)
+            repo.createPermanent(collection)
+            repo[collection].insert_many(data)
+
+    @staticmethod
+    def execute(trial = False):
+        '''Retrieve some data sets'''
+        startTime = datetime.datetime.now()
+
+        repo = FetchData.setup()
 
         datasets = {
             'asafer_vivyee.orchards': 'https://data.cityofboston.gov/resource/8tmm-wjbw.json',
@@ -29,15 +43,12 @@ class FetchData(dml.Algorithm):
         }
 
         for collection, url in datasets.items():
-            response = request.get(url)
-            if response.status_code == 200:
-                data = json.loads(response)
-                repo.dropPermanent(collection)
-                repo.createPermanent(collection)
-                repo[collection].insert_many(data)
+            FetchData.store(repo, url, collection)
 
         repo.logout()
 
         endTime = datetime.datetime.now()
 
         return {"start":startTime, "end":endTime}
+
+FetchData.execute()
