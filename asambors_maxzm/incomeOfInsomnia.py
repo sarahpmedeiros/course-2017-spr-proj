eae94b9f34e4bb4c878d7d0bc941f1b2201e5b84
max <max.mesirow@gmail.com>
2017-02-15 23:59:30 -0500

    started data transformations

---
+++ asambors_maxzm/incomeOfInsomnia.py
@@ -0,0 +1,86 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+class incomeOfInsomnia(dml.Algorithm):
+        contributor = 'asambors_maxzm'
+        read = ['asambors_maxzm.nosleep','asambors_maxzm.ziptoincome','asambors_maxzm.zipcodetolatlong']
+        writes = ['asambors_maxzm.incomeofinsomnia']
+
+
+        @staticmethod
+        def execute(trial=False):
+                startTime = datetime.datetime.now()
+
+                #set up the datebase connection
+                client = dml.pymongo.MongoClient()
+                repo = client.repo
+                repo.authenticate('asambors_maxzm','asambors_maxzm')
+
+                
+                #loads
+                nosleep = repo['asambors_maxzm.nosleep']
+                ziptoincome = repo['asambors_maxzm.ziptoincome']
+                zipcodetolatlong = repo['asambors_maxzm.zipcodetolatlong']
+
+                
+                #run select on the zip lat long dataset 
+
+                
+
+                #map each no sleep person to corosponding zip based on lat long
+
+
+
+
+
+    @staticmethod
+        def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        ''' Create the provenance document describing everything happening
+        in this script. Each run of the script will generate a new
+        document describing that invocation event '''
+
+        # Set up the database connection.
+            client = dml.pymongo.MongoClient()
+            repo = client.repo
+            repo.authenticate('asambors_maxzm', 'asambors_maxzm')
+            doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+            doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+            doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+            doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+            doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+            this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+            resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+            get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+            get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+            doc.wasAssociatedWith(get_found, this_script)
+            doc.wasAssociatedWith(get_lost, this_script)
+            doc.usage(get_found, resource, startTime, None,
+                      {prov.model.PROV_TYPE:'ont:Retrieval',
+                      'ont:Query':'?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                      }
+           )
+            doc.usage(get_lost, resource, startTime, None,
+                      {prov.model.PROV_TYPE:'ont:Retrieval',
+                      'ont:Query':'?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+                    }
+            )
+
+            lost = doc.entity('dat:alice_bob#lost', {prov.model.PROV_LABEL:'Animals Lost', prov.model.PROV_TYPE:'ont:DataSet'})
+            doc.wasAttributedTo(lost, this_script)
+            doc.wasGeneratedBy(lost, get_lost, endTime)
+            doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+
+            found = doc.entity('dat:alice_bob#found', {prov.model.PROV_LABEL:'Animals Found', prov.model.PROV_TYPE:'ont:DataSet'})
+            doc.wasAttributedTo(found, this_script)
+            doc.wasGeneratedBy(found, get_found, endTime)
+            doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+            repo.logout()
+            return doc
+
+
+incomeOfInsomnia.execute()
