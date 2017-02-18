d27f3efc6bedfe2c10e2ecc24eaafb5984ec1cbc
tiger <tigerlei@bu.edu>
2017-02-18 18:12:55 -0500

    craft

--- tigerlei/transformation1.py
+++ tigerlei/transformation1.py
@@ -5,38 +5,45 @@ import prov.model
 import datetime
 import uuid
 
-class example(dml.Algorithm):
-    contributor = 'alice_bob'
-    reads = []
-    writes = ['alice_bob.lost', 'alice_bob.found']
+class transformation1(dml.Algorithm):
+    contributor = 'tigerlei'
+    reads = ['tigerlei.crime1','tigerlei.crime2']
+    writes = ['tigerlei.crime']
 
     @staticmethod
     def execute(trial = False):
-        '''Retrieve some data sets (not using the API here for the sake of simplicity).'''
         startTime = datetime.datetime.now()
 
-        # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
-
-        url = 'http://cs-people.bu.edu/lapets/591/examples/lost.json'
-        response = urllib.request.urlopen(url).read().decode("utf-8")
-        r = json.loads(response)
-        s = json.dumps(r, sort_keys=True, indent=2)
-        repo.dropCollection("lost")
-        repo.createCollection("lost")
-        repo['alice_bob.lost'].insert_many(r)
-        repo['alice_bob.lost'].metadata({'complete':True})
-        print(repo['alice_bob.lost'].metadata())
-
-        url = 'http://cs-people.bu.edu/lapets/591/examples/found.json'
-        response = urllib.request.urlopen(url).read().decode("utf-8")
-        r = json.loads(response)
-        s = json.dumps(r, sort_keys=True, indent=2)
-        repo.dropCollection("found")
-        repo.createCollection("found")
-        repo['alice_bob.found'].insert_many(r)
+        repo.authenticate('tigerlei', 'tigerlei')
+        repo.dropCollection("crime")
+        repo.createCollection("crime")
+        crime1 = []
+        crime2 = []
+        for item in repo['tigerlei.crime1'].find():
+            temp = {}
+            # Project useful part of dataset
+            try:
+                temp['district'] = item['reptdistrict']
+                temp['latitude'] = item['location']['latitude']
+                temp['longitude'] = item['location']['longitude']
+                crime1.append(temp)
+            # If part of data was missing, ignore this record
+            except:
+                pass
+        for item in repo['tigerlei.crime2'].find():
+            temp = {}
+            try:
+                temp['district'] = item['district']
+                temp['latitude'] = item['lat']
+                temp['longitude'] = item['long']
+                crime2.append(temp)
+            except:
+                pass
+        # Union all crime incidents from 2012 to 2016
+        crime = crime1 + crime2
+        repo['tigerlei.crime'].insert_many(crime)
 
         repo.logout()
 
@@ -55,14 +62,14 @@ class example(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('alice_bob', 'alice_bob')
+        repo.authenticate('tigerlei', 'tigerlei')
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
         doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
         doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
         doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 
-        this_script = doc.agent('alg:alice_bob#example', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        this_script = doc.agent('alg:tigerlei#retrieve', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         resource = doc.entity('bdp:wc8w-nujj', {'prov:label':'311, Service Requests', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         get_found = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
         get_lost = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
@@ -93,9 +100,9 @@ class example(dml.Algorithm):
                   
         return doc
 
-example.execute()
-# doc = example.provenance()
-# print(doc.get_provn())
+transformation1.execute()
+#doc = retrieve.provenance()
+#print(doc.get_provn())
 
 #print(json.dumps(json.loads(doc.serialize()), indent=4))
 
