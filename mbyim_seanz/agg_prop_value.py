ba9ee82e0ee6381d1b1b9137513b5d30459ed41e
puzzledsean <puzzledsean@gmail.com>
2017-02-20 00:57:03 -0500

    aggregate on parking ticket violations complete and some code clean

--- mbyim_seanz/agg_prop_value.py
+++ mbyim_seanz/agg_prop_value.py
@@ -1,13 +1,12 @@
-#Transformation #1
-
-
+#Transformation #2
+#aggregates property value by zip code
+#i.e. in 02215, property value totaled 1000000
 import urllib.request
 import json
 import dml
 import prov.model
 import datetime
 import uuid
-
 
 class agg_prop_value(dml.Algorithm):
 	contributor = 'mbyim_seanz'
@@ -18,8 +17,8 @@ class agg_prop_value(dml.Algorithm):
 	def execute(trial = False):
 		#S/O To Professor Lapetz
 		def aggregate(R, f):
-			keys = {r for r in R}
-			return [{key: f([1 for k in R if k == key])} for key in keys]
+			keys = {r[0] for r in R}
+			return [{key: f([int(v) for (k,v) in R if k == key])} for key in keys]
 
 		#list of boston zipcodes from:
 		#http://zipcode.org/city/MA/BOSTON
@@ -27,50 +26,44 @@ class agg_prop_value(dml.Algorithm):
 				"02128","02133","02163","02196", "02199", "02205", "02206", "02212", "02215", "02266", "02283", 
 				"02201", "02203", "02204", "02210", "02211", "02217", "02222", "02241", "02284", "02293", "02295", "02297", "02298"]
 
-
 		startTime = datetime.datetime.now()
 		client = dml.pymongo.MongoClient()
 		repo = client.repo
 
 		# Connect to mongoDB
-		#repo = openDb(getAuth("mbyim_seanz"), getAuth("mbyim_seanz"))
 		repo.authenticate('mbyim_seanz', 'mbyim_seanz')
 
 		property_assessments = repo.mbyim_seanz.property_assessments.find()
 
-		#make the zip_grouped_data
+		#make the aggregated_zip_data
 		zip_data = []
 		for row in property_assessments:
 			prop_dict = dict(row)
 			try:
-				prop_dict_select = prop_dict['zipcode']
-				if prop_dict_select in boston_zips:
-					zip_data.append(prop_dict_select)
+				property_zip_code = prop_dict['zipcode']
+				property_value = prop_dict['av_total']
+				if property_zip_code in boston_zips:
+					zip_data.append([property_zip_code, property_value])
 			except:
-				print('there was an error with the zipcode key')
+				print('There was an error with getting a zipcode key')
 
-		zip_grouped_data = aggregate(zip_data, sum)		
-		print(len(zip_grouped_data))
-		
-		zip_grouped_data_str = str(zip_grouped_data).replace("'",'"')
+		aggregated_zip_data = aggregate(zip_data, sum)		
+		print(aggregated_zip_data)
 
-		zip_jsons = json.loads(zip_grouped_data_str)
+		aggregated_zip_data_str = str(aggregated_zip_data).replace("'",'"')	#small json fix to convert to strings
+
+		zip_jsons = json.loads(aggregated_zip_data_str)
 		s = json.dumps(zip_jsons, sort_keys=True, indent = 2)
 		repo.dropCollection("agg_prop_value")
 		repo.createCollection("agg_prop_value")
-
 		repo['mbyim_seanz.agg_prop_value'].insert_many(zip_jsons)
 		repo['mbyim_seanz.agg_prop_value'].metadata({'complete':True})
 
-
 	@staticmethod
 	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-
 		client = dml.pymongo.MongoClient()
 		repo = client.repo
 		repo.authenticate('mbyim_seanz', 'mbyim_seanz')
-
-
 		doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
 		doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
 		doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
@@ -78,21 +71,16 @@ class agg_prop_value(dml.Algorithm):
 		doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 		doc.add_namespace('mbta', 'http://realtime.mbta.com/developer/api/v2/stopsbyroute')
 
-		this_script = doc.agent('alg:mbyim_seanz#AggPropValue', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+		this_script = doc.agent('alg:mbyim_seanz#agg_prop_value', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 		resource_agg_prop_value = doc.entity('bdp:jsri-cpsq', {'prov:label':'Aggregate Property Assessments', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
-
 		get_agg_prop_value = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-
 		doc.wasAssociatedWith(get_agg_prop_value, this_script)
-
 		doc.usage(get_agg_prop_value, resource_agg_prop_value, startTime, None,
 		          {prov.model.PROV_TYPE:'ont:Retrieval',
 		          'ont:Query':'?format=json' #not sure what this does
 		          }
 		)
-
-
 		agg_prop_value = doc.entity('dat:mbyim_seanz#agg_prop_value', {prov.model.PROV_LABEL:'Aggregated Property Assessments', prov.model.PROV_TYPE:'ont:DataSet'})
 		doc.wasAttributedTo(agg_prop_value, this_script)
 		doc.wasGeneratedBy(agg_prop_value, get_agg_prop_value, endTime)
--- mbyim_seanz/agg_vehicle_tax.py
+++ mbyim_seanz/agg_vehicle_tax.py
@@ -1,5 +1,6 @@
 #Transformation #1
-
+#aggregates number of vehicles taxed by zip code in Boston
+#i.e. 10 vehicles in 02215
 
 import urllib.request
 import json
@@ -8,14 +9,10 @@ import prov.model
 import datetime
 import uuid
 
-
 class agg_vehicle_tax(dml.Algorithm):
 	contributor = 'mbyim_seanz'
 	reads = ['mbyim_seanz.vehicle_tax']
 	writes = ['mbyim_seanz.agg_vehicle_tax']
-
-	
-
 
 	@staticmethod
 	def execute(trial = False):
@@ -37,44 +34,30 @@ class agg_vehicle_tax(dml.Algorithm):
 		repo = client.repo
 
 		# Connect to mongoDB
-		#repo = openDb(getAuth("mbyim_seanz"), getAuth("mbyim_seanz"))
 		repo.authenticate('mbyim_seanz', 'mbyim_seanz')
 
 		vehicle_tax = repo.mbyim_seanz.vehicle_tax.find()
 
-		#make the zip_grouped_data
+		#make the vehicle_zip_aggregate
 		zip_list = []
-		count = 0
-		count1 = 0
 		for row in vehicle_tax:
 			tax_dict = dict(row)
-			#print(tax_dict)
 			tax_dict_modified = tax_dict['zip'].split("-")[0]
 			if tax_dict_modified in boston_zips:
-				count1 += 1
 				zip_list.append(tax_dict_modified)
 			else:
-				count += 1
 				continue
 
-		zip_grouped_data = aggregate(zip_list, sum)
+		vehicle_zip_aggregate = aggregate(zip_list, sum)
+		vehicle_zip_aggregate_str = str(vehicle_zip_aggregate).replace("'",'"') #small json fix to convert to strings
 
-		#print(len(zip_list))
-		#print(zip_grouped_data)
-		#print(count1)
-		#print(count)
-
-
-		zip_grouped_data_str = str(zip_grouped_data).replace("'",'"')
-
-		zip_jsons = json.loads(zip_grouped_data_str)
+		zip_jsons = json.loads(vehicle_zip_aggregate_str)
 		s = json.dumps(zip_jsons, sort_keys=True, indent = 2)
 		repo.dropCollection("agg_vehicle_tax")
 		repo.createCollection("agg_vehicle_tax")
 
 		repo['mbyim_seanz.agg_vehicle_tax'].insert_many(zip_jsons)
 		repo['mbyim_seanz.agg_vehicle_tax'].metadata({'complete':True})
-
 	
 	@staticmethod
 	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
@@ -91,7 +74,7 @@ class agg_vehicle_tax(dml.Algorithm):
 		doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
 		doc.add_namespace('mbta', 'http://realtime.mbta.com/developer/api/v2/stopsbyroute')
 
-		this_script = doc.agent('alg:mbyim_seanz#AggVehicleTax', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+		this_script = doc.agent('alg:mbyim_seanz#agg_vehicle_tax', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 		resource_agg_vehicle_tax = doc.entity('bdp:ww9y-x77a', {'prov:label':'Aggregated Vehicle Tax', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
 
