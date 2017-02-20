98c67a3e3891c116149b9bd7e95923956091e056
Ann Ming Samborski <asambors@bu.edu>
2017-02-19 20:13:00 -0500

    removed unnecessary files, modified cdc fetch file (keeping for testing)

--- asambors_maxzm/dataSets/csv2json.py
+++ asambors_maxzm/dataSets/csv2json.py
@@ -2,15 +2,14 @@ import csv
 import json
 
 #csvfile = open('test.csv','r')
-csvfile = open('zipCodeSallaries.csv','r')
-jsonfile = open('zipCodeSallaries.json','w')
+csvfile = open('zipcodestolatlong.txt','r')
+jsonfile = open('zipcodestolatlong.json','w')
 
-fieldnames = ("zip_code", "income")
+# ZIP,LAT,LNG
+fieldnames = ("zip_code", "lat", "long")
 
-
-reader = csv.DictReader(csvfile,fieldnames,delimiter=',')
+reader = csv.DictReader(csvfile, fieldnames, delimiter = ',')
 for row in reader:
-        json.dump(row,jsonfile)
-        jsonfile.write(',')
-        jsonfile.write('\n')
-
+    json.dump(row, jsonfile)
+    jsonfile.write(',')
+    jsonfile.write('\n')
