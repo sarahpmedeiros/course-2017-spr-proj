d508052b22b76d2443b30408c0314add93919978
rcalleja <rcalleja@bu.edu>
2017-02-21 11:09:52 -0500

    resolving conflicts

---
+++ hschurma_rcalleja/SATByFunding.py
@@ -0,0 +1,155 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+
+
+# Things to Improve:
+# write final database to repo
+# go back and remove schools that don't have all 4 SAT scores
+# edit school names so that the same school is spelled the same way in both datasets
+
+
+def union(R, S):
+    return R + S
+
+
+def difference(R, S):
+    return [t for t in R if t not in S]
+
+
+def intersect(R, S):
+    return [t for t in R if t in S]
+
+
+def project(R, p):
+    return [p(t) for t in R]
+
+
+def select(R, s):
+    return [t for t in R if s(t)]
+
+
+def product(R, S):
+    return [(t, u) for t in R for u in S]
+
+
+def aggregate(R, f):
+    keys = {r[0] for r in R}
+    return [(key, f([v for (k, v) in R if k == key])) for key in keys]
+
+
+class locationByFunding(dml.Algorithm):
+    contributor = 'hschurma_rcalleja'
+    reads = ['hschurma_rcalleja.funding', 'hschurma_rcalleja.SAT']
+    writes = ['hschurma_rcalleja.SAT_funding']
+
+    @staticmethod
+    def execute(trial=False):
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('hschurma_rcalleja', 'hschurma_rcalleja')
+
+        repo.dropPermanent("SAT_funding")
+        repo.createPermanent("SAT_funding")
+
+        # size of SAT data set
+        SAT = list(repo.hschurma_rcalleja.SAT.find())
+        SATlen = len(SAT)
+        #print(SAT)
+
+        # list of schools
+        SATschools = []
+        for i in range(2, SATlen):
+            temp = SAT[i]
+            SATschools.append((temp['FIELD1'].strip(), (temp['FIELD2'], temp['FIELD3'], temp['FIELD4'], temp['FIELD5'])))
+
+        # Dict of School name and Funding
+        funding = list(repo.hschurma_rcalleja.funding.aggregate([{"$project": {"_id": 0, "FIELD2": 1, "FIELD13": 1}}]))
+
+        nameFund = []
+        for i in range(len(funding)):
+            n = funding[i]["FIELD2"].strip()
+            nameFund.append((n, funding[i]["FIELD13"]))
+
+        # print(nameFund)
+        # print(nameLoc)
+        # print(nameFund)
+
+
+        P = product(SATschools, nameFund)
+        #print(P)
+        S = select(P, lambda t: t[0][0] == t[1][0])
+        # print(S)
+        PR = project(S, lambda t: (t[0][0], t[0][1], t[1][1]))
+        print(PR)
+
+
+        # Trim white spaces
+
+    @staticmethod
+    def provenance(doc=prov.model.ProvDocument(), startTime=None, endTime=None):
+        '''Create the provenance document describing everything happening
+            in this script. Each run of the script will generate a new
+            document describing that invocation event.'''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('hschurma_rcalleja', 'hschurma_rcalleja')
+
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/')  # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont',
+                          'http://datamechanics.io/ontology#')  # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/')  # The event log.
+        doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
+
+        this_script = doc.agent('alg:hschurma_rcalleja#retrieve',
+                                {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        resource = doc.entity('bdp:wc8w-nujj',
+                              {'prov:label': '311, Service Requests', prov.model.PROV_TYPE: 'ont:DataResource',
+                               'ont:Extension': 'json'})
+
+        get_found = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+        get_lost = doc.activity('log:uuid' + str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(get_found, this_script)
+        doc.wasAssociatedWith(get_lost, this_script)
+
+        doc.usage(get_found, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval',
+                   'ont:Query': '?type=Animal+Found&$select=type,latitude,longitude,OPEN_DT'
+                   }
+                  )
+        doc.usage(get_lost, resource, startTime, None,
+                  {prov.model.PROV_TYPE: 'ont:Retrieval',
+                   'ont:Query': '?type=Animal+Lost&$select=type,latitude,longitude,OPEN_DT'
+                   }
+                  )
+
+        lost = doc.entity('dat:hschurma_rcalleja#lost',
+                          {prov.model.PROV_LABEL: 'Animals Lost', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(lost, this_script)
+        doc.wasGeneratedBy(lost, get_lost, endTime)
+        doc.wasDerivedFrom(lost, resource, get_lost, get_lost, get_lost)
+
+        found = doc.entity('dat:hschurma_rcalleja#found',
+                           {prov.model.PROV_LABEL: 'Animals Found', prov.model.PROV_TYPE: 'ont:DataSet'})
+        doc.wasAttributedTo(found, this_script)
+        doc.wasGeneratedBy(found, get_found, endTime)
+        doc.wasDerivedFrom(found, resource, get_found, get_found, get_found)
+
+        repo.logout()
+
+        return doc
+
+
+locationByFunding.execute()
+doc = locationByFunding.provenance()
+
