94bb2ba4cf43adf995e6a67d13b167452ea6c7b9
akeelahji <jw0208@bu.edu>
2017-02-21 11:25:07 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-import json
-
-url = "https://data.cityofboston.gov/resource/awu8-dc52.json"
-response = urllib.request.urlopen(url).read().decode("utf-8")
-print(json.dumps(json.loads(response), sort_keys=True, indent=2))
