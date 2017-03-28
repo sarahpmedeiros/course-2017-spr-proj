9b995307838d7ebfe306048c81b71c1c7be11ba1
veev <vivyee@bu.edu>
2017-03-27 22:22:41 -0400

    lol wat r commit msgz

--- asafer_asambors_maxzm_vivyee/shortestMbtaPath.py
+++ asafer_asambors_maxzm_vivyee/shortestMbtaPath.py
@@ -29,14 +29,14 @@ class shortestMbtaPath(dml.Algorithm):
                     time = nx.dijkstra_path_length(G, o_stop, h_stop)
                     min_times.append(time)
                 except nx.NetworkXNoPath:
-                    print('no path found')
+                    # print('no path found')
                     pass
 
         if len(min_times) == 0:
             info['min_travel_time'] = sys.maxsize
         else:
             info['min_travel_time'] = min(min_times)
-            print('YAY')
+            # print(info)
         # print(min_times)
         # print('info is\n' + str(info))
         return info
@@ -46,11 +46,17 @@ class shortestMbtaPath(dml.Algorithm):
     def calculate_distance(lat_1, lon_1, lat_2, lon_2):
         # formula from: http://andrew.hedges.name/experiments/haversine/
         # used R = 3961 miles
+        lat_2 = math.radians(lat_2)
+        lat_1 = math.radians(lat_1)
+        lon_1 = math.radians(lon_1)
+        lon_2 = math.radians(lon_2)
+
+        R = 3961.0
         dlon = lon_1 - lon_2
         dlat = lat_1 - lat_2
         a = math.sin(dlat/2)**2 + (math.cos(lat_2) * math.cos(lat_1) * math.sin(dlon/2)**2)
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-        d = 3961 * c
+        d = R * c
         return d
 
     @staticmethod
@@ -84,8 +90,8 @@ class shortestMbtaPath(dml.Algorithm):
         # weight = distance / mpm = total time it takes from point 1 to 2
         for route in mbta_routes:
             if route['mode'] == 'Subway':
-                mpm = 30 / 60          # miles per minute
-                # print('directions are', len(route['path']['direction']))
+                mpm = 30.0 / 60.0          # miles per minute
+
                 for direction in route['path']['direction']:
                     prev_lat = 0
                     prev_lon = 0
@@ -109,7 +115,10 @@ class shortestMbtaPath(dml.Algorithm):
                             d = shortestMbtaPath.calculate_distance(prev_lat, prev_lon, eval(stop['stop_lat']), eval(stop['stop_lon']))
                             w = d / mpm
                             G.add_edge(prev_stop, stop['stop_id'], weight=w)
-                            # G.add_edge(stop['stop_id'], prev_stop, weight=w)
+                            G.add_edge(stop['stop_id'], prev_stop, weight=w)
+
+                            # if w == 0:
+                            #     print(stop['stop_id'], 'to', prev_stop)
                             # print('current stop:', stop['stop_id'], '; last_stop:', prev_stop, '; weight:', w)
                         prev_lon = eval(stop['stop_lon'])
                         prev_lat = eval(stop['stop_lat'])
