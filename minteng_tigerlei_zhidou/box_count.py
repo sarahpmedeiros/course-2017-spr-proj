92c818811fa4fb691249a9606df6ebb9417b014f
mintengxie <minteng@bu.edu>
2017-03-31 16:55:45 -0400

    changed

--- minteng_tigerlei_zhidou/box_count.py
+++ minteng_tigerlei_zhidou/box_count.py
@@ -41,7 +41,7 @@ class box_count(dml.Algorithm):
         k=10 ####set the #of box
         d1=lat_range[1]-lat_range[0]
         d2=lng_range[1]-lng_range[0]
-
+        radius=max(d1,d2)/k
         result=[]
         for i in range(k):
             for j in range(k):
@@ -53,7 +53,16 @@ class box_count(dml.Algorithm):
                 d={'food':0,'transport':0,'crime':0}
                 for item in a:
                     d[item['type']]+=1
-                if sum(d.values())==0:
+                #increase the radius of food             
+                if d['food'] < 20:
+                    d['food'] = 0
+                    center=[0.5*(left[0]+right[0]),0.5*(left[1]+right[1])]
+                    b=repo['minteng_tigerlei_zhidou.location'].find({'location':{'$geoWithin':{ '$center': [ center, radius] }}})
+                    for item in b:
+                        if item['type']=='food':
+                            d['food']+=1
+
+                if d['crime']==0:
                     continue
                 result.append({'box':box,'count':d})
                     
@@ -65,16 +74,84 @@ class box_count(dml.Algorithm):
             crime.append(r['count']['crime'])
             transport.append(r['count']['transport'])
             food.append(r['count']['food'])
-        c_max=max(crime)+0.001
-        t_max=max(transport)+0.001
-        f_max=max(food)+0.001
-
-        k=5##grade from 1 - 5
+        def get_range(x):
+            xx=sorted(x)
+            k=int(len(xx)/5)
+            x1=xx[0];x2=xx[k];x3=xx[2*k]
+            x4=xx[3*k];x5=xx[4*k];x6=xx[len(xx)-1]
+            return [x1,x2,x3,x4,x5,x6]
+        def get_grade(c,ranges):
+            [x1,x2,x3,x4,x5,x6]=ranges
+            if c==0:
+                return 1
+            elif x1<=c<x2:
+                return 1
+            elif x2<=c<x3:
+                return 2
+            elif x3<=c<x4:
+                return 3
+            elif x4<=c<x5:
+                return 4
+            else:
+                return 5
+        c=get_range(crime)
+        t=get_range(transport)
+        f=get_range(food)
         for r in result:
-            r1=int(r['count']['crime']/c_max*k)+1
-            r2=int(r['count']['food']/f_max*k)+1
-            r3=int(r['count']['transport']/t_max*k)+1
+            r1=get_grade(r['count']['crime'],c)
+            r2=get_grade(r['count']['food'],f)
+            r3=get_grade(r['count']['transport'],t)
             r['grade']={'crime':r1,'food':r2, 'transport':r3}
+        #reverse grade for crime:
+        for i in result:
+            if i['grade']['crime']==1:
+                i['grade']['crime']=5
+            elif i['grade']['crime']==2:
+                i['grade']['crime']=4
+            elif i['grade']['crime']==4:
+                i['grade']['crime']=2
+            elif i['grade']['crime']==5:
+                i['grade']['crime']=1
+        #find the rent and the grade for rent
+        def find_rent(zipcode):
+            rent=repo['minteng_tigerlei_zhidou.rent'].find()
+            for i in rent:
+                if i['postal_code']==zipcode:
+                    return i['area'],i['avg_rent']
+            area=gmaps.geocode(zipcode)[0]['formatted_address'].split()[0]
+            rent=repo['minteng_tigerlei_zhidou.rent'].find()
+            for j in rent:
+                if j['area']==area:
+                    return j['area'],j['avg_rent']
+            return "Not found", "Not found"
+
+        for i in result:
+            center=[0.5*(i['box'][0][0]+i['box'][1][0]),.5*(i['box'][0][1]+i['box'][1][1])]
+            zipcode=(gmaps.reverse_geocode(center)[0]['formatted_address'].split()[-2].replace(",",""))
+            if(len(zipcode))<5:
+                zipcode='Not found'
+            i['postal_code']=zipcode
+            i['area'],i['avg_rent']=find_rent(zipcode)
+        rr=[]
+        for i in result:
+            if i['avg_rent']!='Not found':
+                rr.append(i['avg_rent'])
+        r=get_range(rr)
+        for i in result:
+            if i['avg_rent']=='Not found':
+                i['grade']['rent']='Not found'
+            else:
+                i['grade']['rent']=get_grade(i['avg_rent'],r)
+        #reverse grade for rent:
+        for i in result:
+            if i['grade']['rent']==1:
+                i['grade']['rent']=5
+            elif i['grade']['rent']==2:
+                i['grade']['rent']=4
+            elif i['grade']['rent']==4:
+                i['grade']['rent']=2
+            elif i['grade']['rent']==5:
+                i['grade']['rent']=1
 
         repo.dropCollection("box_count")
         repo.createCollection("box_count")
@@ -108,6 +185,7 @@ class box_count(dml.Algorithm):
         this_script = doc.agent('alg:minteng_tigerlei_zhidou#box_count', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         
         ### data : box_count
+        #derive from location
         box_count_resource = doc.entity('dbr:minteng_tigerlei_zhidou#location', {'prov:label':'Locations with tag and infomation', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         
         get_box_count = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
@@ -121,7 +199,20 @@ class box_count(dml.Algorithm):
         doc.wasGeneratedBy(box_count, get_box_count, endTime)
         doc.wasDerivedFrom(box_count, box_count_resource, get_box_count, get_box_count, get_box_count)
 
+
+        #derive from rent
+        box_count_resource2 = doc.entity('dbr:minteng_tigerlei_zhidou#rent', {'prov:label':'Rent data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
         
+        get_box_count = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        doc.wasAssociatedWith(get_box_count, this_script)
+        doc.usage(get_box_count, box_count_resource2, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval'})
+
+
+        box_count = doc.entity('dat:minteng_tigerlei_zhidou#box_count', {prov.model.PROV_LABEL:'box count and grade', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(box_count, this_script)
+        doc.wasGeneratedBy(box_count, get_box_count, endTime)
+        doc.wasDerivedFrom(box_count, box_count_resource2, get_box_count, get_box_count, get_box_count)
         repo.logout()
                   
         return doc
