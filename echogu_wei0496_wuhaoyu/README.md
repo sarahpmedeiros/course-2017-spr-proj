577e432440cc0a6fe62cfbcac960ba10725ade82
lingyigu <echogu@bu.edu>
2017-04-01 13:26:18 -0400

    update retrieveData.py

--- echogu_wei0496_wuhaoyu/README.md
+++ echogu_wei0496_wuhaoyu/README.md
@@ -1,9 +1,10 @@
 # Project 2: Modeling, Optimization, and Statistical Analysis
-Team: [Lingyi Gu][lyg], [Wei Wei][ww]
+Team: [Lingyi Gu][lyg], [Wei Wei][ww], [Haoyu Wu][hyw]
 
 The [Boston Public Schools][bps] has announced a [Transportation Challenge][tc].
 
 [lyg]: https://github.com/lingyigu
 [ww]: https://github.com/wei0496
+[hyw]: https://github.com/wuhaoyujerry
 [bps]: http://www.bostonpublicschools.org/
 [tc]: http://bostonpublicschools.org/transportationchallenge
--- echogu_wei0496_wuhaoyu/retrieveData.py
+++ echogu_wei0496_wuhaoyu/retrieveData.py
@@ -9,9 +9,9 @@ import datetime
 import uuid
 
 class retrieveData(dml.Algorithm):
-    contributor = 'echogu_wei0496'
+    contributor = 'echogu_wei0496_wuhaoyu'
     reads = []
-    writes = ['echogu_wei0496.buses', 'echogu_wei0496.grade_safe_distance', 'echogu_wei0496.safety_scores', 'echogu_wei0496.schools', 'echogu_wei0496.students']
+    writes = ['echogu_wei0496_wuhaoyu.buses', 'echogu_wei0496_wuhaoyu.grade_safe_distance', 'echogu_wei0496_wuhaoyu.safety_scores', 'echogu_wei0496_wuhaoyu.schools', 'echogu_wei0496_wuhaoyu.students']
 
     @staticmethod
     def execute(trial=False):
@@ -22,7 +22,7 @@ class retrieveData(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('echogu_wei0496', 'echogu_wei0496')
+        repo.authenticate('echogu_wei0496_wuhaoyu', 'echogu_wei0496_wuhaoyu')
 
         # buses
         url = 'http://datamechanics.io/data/_bps_transportation_challenge/buses.json'
@@ -30,9 +30,9 @@ class retrieveData(dml.Algorithm):
         r = json.loads(response)
         repo.dropCollection("buses")
         repo.createCollection("buses")
-        repo['echogu_wei0496.buses'].insert_many(r)
-        repo['echogu_wei0496.buses'].metadata({'complete': True})
-        print(repo['echogu_wei0496.buses'].metadata())
+        repo['echogu_wei0496_wuhaoyu.buses'].insert_many(r)
+        repo['echogu_wei0496_wuhaoyu.buses'].metadata({'complete': True})
+        print(repo['echogu_wei0496_wuhaoyu.buses'].metadata())
 
         # grade-safe-distance
         url = 'http://datamechanics.io/data/_bps_transportation_challenge/grade-safe-distance.json'
@@ -40,9 +40,9 @@ class retrieveData(dml.Algorithm):
         r = json.loads(response)
         repo.dropCollection("grade_safe_distance")
         repo.createCollection("grade_safe_distance")
-        repo['echogu_wei0496.grade_safe_distance'].insert(r)
-        repo['echogu_wei0496.grade_safe_distance'].metadata({'complete': True})
-        print(repo['echogu_wei0496.grade_safe_distance'].metadata())
+        repo['echogu_wei0496_wuhaoyu.grade_safe_distance'].insert(r)
+        repo['echogu_wei0496_wuhaoyu.grade_safe_distance'].metadata({'complete': True})
+        print(repo['echogu_wei0496_wuhaoyu.grade_safe_distance'].metadata())
 
         # safety-scores
         url = 'http://datamechanics.io/data/_bps_transportation_challenge/safety-scores.json'
@@ -50,9 +50,9 @@ class retrieveData(dml.Algorithm):
         r = json.loads(response)
         repo.dropCollection("safety_scores")
         repo.createCollection("safety_scores")
-        repo['echogu_wei0496.safety_scores'].insert_many(r)
-        repo['echogu_wei0496.safety_scores'].metadata({'complete': True})
-        print(repo['echogu_wei0496.safety_scores'].metadata())
+        repo['echogu_wei0496_wuhaoyu.safety_scores'].insert_many(r)
+        repo['echogu_wei0496_wuhaoyu.safety_scores'].metadata({'complete': True})
+        print(repo['echogu_wei0496_wuhaoyu.safety_scores'].metadata())
 
         # schools
         url = 'http://datamechanics.io/data/_bps_transportation_challenge/schools.json'
@@ -60,9 +60,9 @@ class retrieveData(dml.Algorithm):
         r = json.loads(response)
         repo.dropCollection("schools")
         repo.createCollection("schools")
-        repo['echogu_wei0496.schools'].insert_many(r)
-        repo['echogu_wei0496.schools'].metadata({'complete': True})
-        print(repo['echogu_wei0496.schools'].metadata())
+        repo['echogu_wei0496_wuhaoyu.schools'].insert_many(r)
+        repo['echogu_wei0496_wuhaoyu.schools'].metadata({'complete': True})
+        print(repo['echogu_wei0496_wuhaoyu.schools'].metadata())
 
         # students
         url = 'http://datamechanics.io/data/_bps_transportation_challenge/students.json'
@@ -70,9 +70,9 @@ class retrieveData(dml.Algorithm):
         r = json.loads(response)
         repo.dropCollection("students")
         repo.createCollection("students")
-        repo['echogu_wei0496.students'].insert_many(r)
-        repo['echogu_wei0496.students'].metadata({'complete': True})
-        print(repo['echogu_wei0496.students'].metadata())
+        repo['echogu_wei0496_wuhaoyu.students'].insert_many(r)
+        repo['echogu_wei0496_wuhaoyu.students'].metadata({'complete': True})
+        print(repo['echogu_wei0496_wuhaoyu.students'].metadata())
 
         repo.logout()
 
@@ -90,7 +90,7 @@ class retrieveData(dml.Algorithm):
         # Set up the database connection.
         client = dml.pymongo.MongoClient()
         repo = client.repo
-        repo.authenticate('echogu_wei0496', 'echogu_wei0496')
+        repo.authenticate('echogu_wei0496_wuhaoyu', 'echogu_wei0496_wuhaoyu')
 
         # create document object and define namespaces
         doc.add_namespace('alg', 'http://datamechanics.io/algorithm/')  # The scripts are in <folder>#<filename> format.
@@ -100,7 +100,7 @@ class retrieveData(dml.Algorithm):
         doc.add_namespace('bps', 'http://datamechanics.io/data/_bps_transportation_challenge/')
 
         # define entity to represent resources
-        this_script = doc.agent('alg:echogu_wei0496#retrieveData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
+        this_script = doc.agent('alg:echogu_wei0496_wuhaoyu#retrieveData', {prov.model.PROV_TYPE: prov.model.PROV['SoftwareAgent'], 'ont:Extension': 'py'})
         resource_buses = doc.entity('bps:buses', {'prov:label': 'buses', prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension': 'json'})
         resource_grade_safe_distance = doc.entity('bps:grade_safe_distance', {'prov:label': 'grade_safe_distance', prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension': 'json'})
         resource_safety_scores = doc.entity('bps:safety_scores', {'prov:label': 'safety_scores', prov.model.PROV_TYPE: 'ont:DataResource', 'ont:Extension': 'json'})
@@ -129,27 +129,27 @@ class retrieveData(dml.Algorithm):
         doc.usage(get_students, resource_students, startTime, None, {prov.model.PROV_TYPE: 'ont:Retrieval'})
 
         # for the data obtained, indicate that the entity was attributed to what agent, was generated by which activity and was derived from what entity
-        buses = doc.entity('dat:echogu_wei0496#buses', {prov.model.PROV_LABEL: 'buses', prov.model.PROV_TYPE: 'ont:DataSet'})
+        buses = doc.entity('dat:echogu_wei0496_wuhaoyu#buses', {prov.model.PROV_LABEL: 'buses', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(buses, this_script)
         doc.wasGeneratedBy(buses, get_buses, endTime)
         doc.wasDerivedFrom(buses, resource_buses, get_buses, get_buses, get_buses)
 
-        grade_safe_distance = doc.entity('dat:echogu_wei0496#grade_safe_distance', {prov.model.PROV_LABEL: 'grade_safe_distance', prov.model.PROV_TYPE: 'ont:DataSet'})
+        grade_safe_distance = doc.entity('dat:echogu_wei0496_wuhaoyu#grade_safe_distance', {prov.model.PROV_LABEL: 'grade_safe_distance', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(grade_safe_distance, this_script)
         doc.wasGeneratedBy(grade_safe_distance, get_grade_safe_distance, endTime)
         doc.wasDerivedFrom(grade_safe_distance, resource_grade_safe_distance, resource_grade_safe_distance, resource_grade_safe_distance, resource_grade_safe_distance)
 
-        safety_scores = doc.entity('dat:echogu_wei0496#safety_scores', {prov.model.PROV_LABEL: 'safety_scores', prov.model.PROV_TYPE: 'ont:DataSet'})
+        safety_scores = doc.entity('dat:echogu_wei0496_wuhaoyu#safety_scores', {prov.model.PROV_LABEL: 'safety_scores', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(safety_scores, this_script)
         doc.wasGeneratedBy(safety_scores, get_safety_scores, endTime)
         doc.wasDerivedFrom(safety_scores, resource_safety_scores, get_safety_scores, get_safety_scores, get_safety_scores)
 
-        schools = doc.entity('dat:echogu_wei0496#schools', {prov.model.PROV_LABEL: 'schools', prov.model.PROV_TYPE: 'ont:DataSet'})
+        schools = doc.entity('dat:echogu_wei0496_wuhaoyu#schools', {prov.model.PROV_LABEL: 'schools', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(schools, this_script)
         doc.wasGeneratedBy(schools, get_schools, endTime)
         doc.wasDerivedFrom(schools, resource_schools, get_schools, get_schools, get_schools)
 
-        students = doc.entity('dat:echogu_wei0496#students', {prov.model.PROV_LABEL: 'students', prov.model.PROV_TYPE: 'ont:DataSet'})
+        students = doc.entity('dat:echogu_wei0496_wuhaoyu#students', {prov.model.PROV_LABEL: 'students', prov.model.PROV_TYPE: 'ont:DataSet'})
         doc.wasAttributedTo(students, this_script)
         doc.wasGeneratedBy(students, get_students, endTime)
         doc.wasDerivedFrom(students, resource_students, get_students, get_students, get_students)
@@ -158,7 +158,7 @@ class retrieveData(dml.Algorithm):
 
         return doc
 
-retrieveData.execute()
+# retrieveData.execute()
 # doc = retrieveData.provenance()
 # print(doc.get_provn())
 # print(json.dumps(json.loads(doc.serialize()), indent=4))
