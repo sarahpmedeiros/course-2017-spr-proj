fc2bab9ee570180dab6f6d2067283203aeebb056
Pauline Ramirez <paulineramirez@dhcp-wifi-8021x-168-122-14-251.bu.edu>
2017-04-01 14:26:49 -0400

    added initial files

---
+++ pgr_syquiac/correlation1.py
@@ -0,0 +1,16 @@
+'''
+Pauline Ramirez & Carlos Syquia
+correlation1.py
+
+Calculates the correlation between x and y 
+
+'''
+
+
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+import sodapy
---
+++ pgr_syquiac/correlation2.py
@@ -0,0 +1,16 @@
+'''
+Pauline Ramirez & Carlos Syquia
+retrieveData.py
+
+Calculates the correlation between x and y 
+
+'''
+
+
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime
+import uuid
+import sodapy
