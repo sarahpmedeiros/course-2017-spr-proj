1c72af4338200890d13fdbe6347d87e458b72fca
Alex Rosenfeld <alexrosenfeld10@gmail.com>
2017-04-03 22:03:04 -0400

    safe_points.py

---
+++ ajr10_chamathd_williami/safe_points.py
@@ -0,0 +1,110 @@
+import json
+import dml
+import prov.model
+import datetime
+import random
+import uuid
+from numpy.random import choice
+from shapely.geometry import Polygon, Point, LinearRing
+
+def closest_point_on_border(border, pt):
+    poly = Polygon(border)
+    point = Point(pt)
+
+    pol_ext = LinearRing(poly.exterior.coords)
+    d = pol_ext.project(point)
+    p = pol_ext.interpolate(d)
+    closest_point_coords = list(p.coords)[0]
+    return closest_point_coords
+
+class safe_points(dml.Algorithm):
+    contributor = 'ajr10_chamathd_williami'
+    reads = ['ajr10_chamathd_williami.transformation1']
+    writes = ['ajr10_chamathd_williami.safe_points']
+
+    @staticmethod
+    def execute(trial = False):
+        '''Retrieve some data sets for the MongoDB collection.'''
+        startTime = datetime.datetime.now()
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('ajr10_chamathd_williami', 'ajr10_chamathd_williami')
+
+        # Perform initialization for the new repository
+        colName = "ajr10_chamathd_williami.safe_points"
+        repo.dropCollection(colName)
+        repo.createCollection(colName)
+
+        # Operational code here:
+
+        means_data = repo["ajr10_chamathd_williami.transformation1"]
+        sea_level_five_data = repo["ajr10_chamathd_williami.sea_level_five"]
+        sea_level_seven_data = repo["ajr10_chamathd_williami.sea_level_seven"]
+        
+        safe_pts = []
+        for mean in means_data:
+            safe_pts += closest_point_on_border(mean, POLYGON)
+        
+        # Logout and end
+        repo.logout()
+
+        endTime = datetime.datetime.now()
+
+        return {"start":startTime, "end":endTime}
+    
+    @staticmethod
+    def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+        '''
+        Create the provenance document describing everything happening
+        in this script. Each run of the script will generate a new
+        document describing that invocation event.
+        '''
+
+        # Set up the database connection.
+        client = dml.pymongo.MongoClient()
+        repo = client.repo
+        repo.authenticate('ajr10_chamathd_williami', 'ajr10_chamathd_williami')
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+        doc.add_namespace('acw', 'ajr10_chamathd_williami')
+
+        this_script = doc.agent('alg:ajr10_chamathd_williami#append_polygon_and_centerpoint', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+        neighborhood_area_boston_res = doc.entity('acw:neighborhood_area_boston', {'prov:label':'Boston Neighborhood Population Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        neighborhood_area_cambridge_res = doc.entity('acw:neighborhood_area_cambridge', {'prov:label':'Cambridge Neighborhood Population Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+
+        get_neighborhood_area_boston = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        get_neighborhood_area_cambridge = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+
+        doc.wasAssociatedWith(get_neighborhood_area_boston, this_script)
+        doc.wasAssociatedWith(get_neighborhood_area_cambridge, this_script)
+        
+        doc.usage(get_neighborhood_area_boston, neighborhood_area_boston_res, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Neighborhood+Area+Boston'
+                  }
+                  )
+        doc.usage(get_neighborhood_area_cambridge, neighborhood_area_cambridge_res, startTime, None,
+                  {prov.model.PROV_TYPE:'ont:Retrieval',
+                  'ont:Query':'?type=Neighborhood+Area+Cambridge'
+                  }
+                  )
+
+        neighborhood_info = doc.entity('dat:ajr10_chamathd_williami#neighborhood_info', {prov.model.PROV_LABEL:'Boston-Area Neighborhood Information', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(neighborhood_info, this_script)
+        doc.wasGeneratedBy(neighborhood_info, get_neighborhood_area_boston, endTime)
+        doc.wasGeneratedBy(neighborhood_info, get_neighborhood_area_cambridge, endTime)
+        
+        repo.logout()
+                  
+        return doc
+
+safe_points.execute()
+##doc = safe_points.provenance()
+##print(doc.get_provn())
+##print(json.dumps(json.loads(doc.serialize()), indent=4))
+
+## eof
