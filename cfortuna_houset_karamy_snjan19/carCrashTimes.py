52260b99087ff413797a2576e66b85b43a7be497
fortunac <cfortuna@bu.edu>
2017-04-05 23:57:43 -0400

    Trial mode for car crash times

--- cfortuna_houset_karamy_snjan19/carCrashTimes.py
+++ cfortuna_houset_karamy_snjan19/carCrashTimes.py
@@ -79,6 +79,7 @@ class carCrashTimes(dml.Algorithm):
         return {"start": startTime, "end": endTime}
 
     """Provenance of this Document"""
+    """Provenance of this Document"""
     @staticmethod
     def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
         '''
@@ -91,52 +92,42 @@ class carCrashTimes(dml.Algorithm):
         client = dml.pymongo.MongoClient()
         repo = client.repo
         repo.authenticate('cfortuna_houset_karamy_snjan19', 'cfortuna_houset_karamy_snjan19')
+        
+        doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+        doc.add_namespace('dat', 'http://datamechanics.io/data/') # The data sets are in <user>#<collection> format.
+        doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+        doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
         doc.add_namespace('bdp', 'https://data.cityofboston.gov/resource/')
         doc.add_namespace('mag', 'https://data.mass.gov/resource/')
+        doc.add_namespace('car', 'http://datamechanics.io/data/cfortuna_houset_karamy_snjan19/')
 
         this_script = doc.agent('alg:cfortuna_houset_karamy_snjan19#retrieveData', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
         
-        wazeResource = doc.entity('bdp:dih6-az4h', {'prov:label':'Waze Traffic Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        hospitalsResource = doc.entity('bdp:u6fv-m8v4', {'prov:label':'Boston Hospitals Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
-        streetResource = doc.entity('mag:ms23-5ubn', {'prov:label':'Boston Streets Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
 
-        getWazeTrafficData = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        getBostonHospitalsData = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
-        getBostonStreetsData = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+        carCrashResource = doc.entity('car:CarCrashData', {'prov:label':'Car Crash Data', prov.model.PROV_TYPE:'ont:DataResource', 'ont:Extension':'json'})
+        crashTimesResource = doc.entity('dat:cfortuna_houset_karamy_snjan19#CrashTimes', {'prov:label':'Crash Time Data', prov.model.PROV_TYPE:'ont:DataResource'})
 
-        doc.wasAssociatedWith(getWazeTrafficData, this_script)
-        doc.wasAssociatedWith(getBostonHospitalsData, this_script)
-        doc.wasAssociatedWith(getBostonStreetsData, this_script)
+        getCarCrashData = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)  
+        getCrashTimes= doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime) 
         
-        doc.usage(getWazeTrafficData, wazeResource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  #'ont:Query':'?type=Neighborhood+Area+Boston'
-                  }
-                  )
-        doc.usage(getBostonHospitalsData, hospitalsResource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  #'ont:Query':'?type=Neighborhood+Area+Cambridge'
-                  }
-                  )
-        doc.usage(getBostonStreetsData, streetResource, startTime, None,
-                  {prov.model.PROV_TYPE:'ont:Retrieval',
-                  #'ont:Query':'?type=Neighborhood+Area+Cambridge'
-                  }
-                  )
-        WazeTrafficData = doc.entity('dat:cfortuna_houset_karamy_snjan19#WazeTrafficData', {prov.model.PROV_LABEL:'Waze Traffic Data', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(WazeTrafficData, this_script)
-        doc.wasGeneratedBy(WazeTrafficData, getWazeTrafficData, endTime)
-        doc.wasDerivedFrom(WazeTrafficData, wazeResource, getWazeTrafficData, getWazeTrafficData, getWazeTrafficData)
 
-        BostonHospitalsData = doc.entity('dat:cfortuna_houset_karamy_snjan19#BostonHospitalsData', {prov.model.PROV_LABEL:'Boston Hospitals Data', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(BostonHospitalsData, this_script)
-        doc.wasGeneratedBy(BostonHospitalsData, getBostonHospitalsData, endTime)
-        doc.wasDerivedFrom(BostonHospitalsData, hospitalsResource, getBostonHospitalsData, getBostonHospitalsData, getBostonHospitalsData)
+        doc.wasAssociatedWith(getCarCrashData, this_script)
+        doc.wasAssociatedWith(getCrashTimes, this_script)
 
-        BostonStreetsData = doc.entity('dat:cfortuna_houset_karamy_snjan19#BostonStreetsData', {prov.model.PROV_LABEL:'Boston Streets Data', prov.model.PROV_TYPE:'ont:DataSet'})
-        doc.wasAttributedTo(BostonStreetsData, this_script)
-        doc.wasGeneratedBy(BostonStreetsData, getBostonStreetsData, endTime)
-        doc.wasDerivedFrom(BostonStreetsData, streetResource, getBostonStreetsData, getBostonStreetsData, getBostonStreetsData)
+        doc.usage(getCarCrashData, carCrashResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        doc.usage(getCrashTimes, crashTimesResource, startTime, None, {prov.model.PROV_TYPE:'ont:Retrieval'})
+        '''I NEED INPUT ON THE USAGE PART FOR getCrashTimes PLEASE'''
+
+        CarCrashData = doc.entity('dat:cfortuna_houset_karamy_snjan19#CarCrashData', {prov.model.PROV_LABEL:'Car Crash Data', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(CarCrashData, this_script)
+        doc.wasGeneratedBy(CarCrashData, getCarCrashData, endTime)
+        doc.wasDerivedFrom(CarCrashData, carCrashResource, getCarCrashData, getCarCrashData, getCarCrashData)
+
+        CrashTimes = doc.entity('dat:cfortuna_houset_karamy_snjan19#CrashTimes', {prov.model.PROV_LABEL:'Crash Time Data', prov.model.PROV_TYPE:'ont:DataSet'})
+        doc.wasAttributedTo(CrashTimes, this_script)
+        doc.wasGeneratedBy(CrashTimes, getCrashTimes, endTime)            
+        doc.wasDerivedFrom(CrashTimes, crashTimesResource, getCrashTimes, getCrashTimes, getCrashTimes)
+
 
         repo.logout()
                   
