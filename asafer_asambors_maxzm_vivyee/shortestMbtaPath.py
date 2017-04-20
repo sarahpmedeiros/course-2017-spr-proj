8175402213316dc2858c2d5f8be1e50917973dad
veev <vivyee@bu.edu>
2017-04-20 19:00:42 -0400

    fixes

--- asafer_asambors_maxzm_vivyee/shortestMbtaPath.py
+++ asafer_asambors_maxzm_vivyee/shortestMbtaPath.py
@@ -41,34 +41,34 @@ class shortestMbtaPath(dml.Algorithm):
         # obesity_bus_stops = [ stop['stop_id'] for stop in info['obesity_locations']['stops'] if stop['mode'] != 'Subway' ]
         # healthy_bus_stops = [ stop['stop_id'] for stop in info['healthy_locations']['stops'] if stop['mode'] != 'Subway' ]
 
-        origin_long = info['obesity_locations']['obesity']['geolocation']['rect_lon']
-        origin_lat = info['obesity_locations']['obesity']['geolocation']['rect_lat']
+        # origin_long = info['obesity_locations']['obesity']['geolocation']['rect_lon']
+        # origin_lat = info['obesity_locations']['obesity']['geolocation']['rect_lat']
 
-        dest_lat = info['healthy_locations']['healthy_locations']['rect_location'][0]
-        dest_long = info['healthy_locations']['healthy_locations']['rect_location'][1]
+        # dest_lat = info['healthy_locations']['healthy_locations']['rect_location'][0]
+        # dest_long = info['healthy_locations']['healthy_locations']['rect_location'][1]
 
-        # print("Origin lat long {} {} and destination lat long {} {}".format(origin_lat, origin_long, dest_lat, dest_long))
+        # # print("Origin lat long {} {} and destination lat long {} {}".format(origin_lat, origin_long, dest_lat, dest_long))
 
-        base_link = "https://maps.googleapis.com/maps/api/directions/json?origin=" 
-        params = str(origin_lat) + "," + str(origin_long) + "&destination=" + str(dest_lat) + "," + str(dest_long) 
-        mode = "&mode=transit&transit_mode=bus"
-        key = "&key=AIzaSyACe_alFTeQloNBbdF1mIDguNBoLVYZAnc"
+        # base_link = "https://maps.googleapis.com/maps/api/directions/json?origin=" 
+        # params = str(origin_lat) + "," + str(origin_long) + "&destination=" + str(dest_lat) + "," + str(dest_long) 
+        # mode = "&mode=transit&transit_mode=bus"
+        # key = "&key=AIzaSyACe_alFTeQloNBbdF1mIDguNBoLVYZAnc"
 
-        link = base_link + params + mode + key
+        # link = base_link + params + mode + key
 
-        # print("\nREQUEST URL IS {} ".format(link))
+        # # print("\nREQUEST URL IS {} ".format(link))
 
-        response = rq(method="GET", url=link)
-        raw_json = response.json()
+        # response = rq(method="GET", url=link)
+        # raw_json = response.json()
 
-        for route in raw_json['routes']:
-            sum = 0
-            for leg in route['legs']:
-                time_in_seconds = leg['duration']['value']
-                sum += time_in_seconds
+        # for route in raw_json['routes']:
+        #     sum = 0
+        #     for leg in route['legs']:
+        #         time_in_seconds = leg['duration']['value']
+        #         sum += time_in_seconds
 
-            sum /= 60.0
-            min_times.append(sum)
+        #     sum /= 60.0
+        #     min_times.append(sum)
  
         # print("\nRESPONSE IS {}".format(response.json()))
         # print("\nSTATUS CODE IS {}".format(response.status_code))
