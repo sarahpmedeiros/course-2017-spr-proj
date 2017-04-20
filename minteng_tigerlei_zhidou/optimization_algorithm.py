9d76b7afdf08798bfdee48bf9af2ea60b6784aab
mintengxie <minteng@bu.edu>
2017-04-19 20:38:59 -0400

    add web app

---
+++ minteng_tigerlei_zhidou/optimization_algorithm.py
@@ -0,0 +1,70 @@
+import urllib.request
+import json
+import dml
+import prov.model
+import datetime 
+import uuid
+import time
+
+def get_result(f,t,s,r):
+	contributor = 'minteng_tigerlei_zhidou'
+	reads = ['minteng_tigerlei_zhidou.box_count']
+	writes = ['minteng_tigerlei_zhidou.optimization_result']
+	client = dml.pymongo.MongoClient()
+	repo = client.repo
+	repo.authenticate('minteng_tigerlei_zhidou', 'minteng_tigerlei_zhidou')
+
+	# user will set the grade they want
+	transport=t
+	food=f	
+	safety=s
+	rent=r
+
+	#find the fitted area
+	def if_fitted(A,requirement):#[t,f,s,r] is the requirment/standred
+		[t1,f1,s1,r1]=A
+		[t,f,s,r]=requirement
+		if r1=='Not found':
+			return False
+		if t1>=t and f1>=f and s1>=s and r1>=r:
+			return True
+		return False
+	def get_dist(A,requirement):
+		[t1,f1,s1,r1]=A
+		[t,f,s,r]=requirement
+		if r1=='Not found':
+			return 1000
+		return ((t1-t)**2+(f1-f)**2+(s1-s)**2+(r1-r)**2)**0.5
+
+	res=[]    
+	a=repo['minteng_tigerlei_zhidou.box_count'].find()
+	for i in a:
+		grade=[i['grade']['transport'],i['grade']['food'],i['grade']['safety'],i['grade']['rent']]
+		if if_fitted(grade,[transport,food,safety,rent]):
+			temp=i
+			temp['rating']=sum(i['grade'].values())
+			res.append(temp)
+		else:
+			temp=i
+			temp['rating']=get_dist(grade,[transport,food,safety,rent])*-1
+			res.append(temp)
+
+
+	#return top fitted
+	result=sorted(res, key=lambda x: x['rating'], reverse=True)
+
+	for i in result:
+		i['center']=[(i['box'][0][0]+i['box'][1][0])/2,(i['box'][0][1]+i['box'][1][1])/2]
+
+	# print('Top fitted areas:')
+	# top=5
+	# for i in range(top):
+	# 	print("Center:",result[i]['center'])
+	# 	print("Bound:",result[i]['box'])
+	# 	print("Area:",result[i]['area'],result[i]['postal_code'],"   Avg rent:",result[i]['avg_rent'])
+	# 	print("Grades:",result[i]['grade'],'\n')
+
+
+	return result
+
+#get_result(3,4,3,4)
---
+++ minteng_tigerlei_zhidou/web.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import dml
+import optimization_algorithm
+
+app = Flask(__name__)
+
+info=None
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/optimize/", methods=["GET"])
+def optimize():
+    return render_template('optimize.html')
+
+@app.route("/optimize-result/", methods=["GET"])
+def optimize_result():
+
+    food=request.args["Food"]
+    transport=request.args['Transport']
+    safe=request.args['Safety']
+    rent=request.args['Rent']
+    #print(food,transport,safe,rent)
+    result=optimization_algorithm.get_result(int(food),int(transport),int(safe),int(rent))
+    res=result[:5]
+    for i in res:
+        i['grade']=[i['grade']['food'],i['grade']['transport'],i['grade']['safety'],i['grade']['rent']]
+    global info
+    info=res
+    return render_template('optimize-result.html',f=food,t=transport,s=safe,r=rent,res=res)
+
+@app.route("/map/")
+def get_map():
+    global info
+    return render_template('map.html',a=info[0],b=info[1],c=info[2],d=info[3],e=info[4])
+
+@app.route("/static-analysis/")
+def static_analysis():
+    return render_template('static-analysis.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
